9165fbda13167b1694b16c70c306f8bf7380ab94
Mégane Diéval <meganedieval@gmail.com>
2020-09-19 14:06:54 +0200

    data cleaned

---
+++ numeric_applications.py
@@ -0,0 +1,5 @@
+
+from download import download
+import pandas as pd
+#data links
+https://github.com/MegDie/advanced_lm_introduction/blob/master/datasets/Donnees_Comptages_Velos_Totem_Albert_1er_verbose.csv
