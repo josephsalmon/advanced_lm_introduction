e56eea035987196c961755a95fab46150301ad65
josephsalmon <joseph.salmon.taff@gmail.com>
2020-11-22 10:46:00 +0100

    cleaning for update on course

--- data_analysis.py
+++ data_analysis.py
@@ -7,32 +7,30 @@ import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from scipy.stats import t
-
+import calendar
 import patsy
 
-
-
-#data links
+# Data links
 url1 = "https://github.com/MegDie/advanced_lm_introduction/blob/master/datasets/Donnees_comptage.ods"
 url2 = "https://github.com/MegDie/advanced_lm_introduction/blob/master/datasets/crash_bikes.csv"
 
-#data download
+# Data download
 path_target1 = "datasets/datasets/Donnees_comptage.ods"
 path_target2 = "datasets/crash_bikes.csv"
 download(url1, path_target1, replace=False)
 download(url2, path_target2, replace=False)
 
-#clean data
+# Clean data
 df_bikes = pd.read_csv(path_target2, na_values="", converters={'data': str, 'heure': str})
 df_bikes.heure.unique()
-df_bikes['heure']=df_bikes['heure'].replace('', np.nan)
+df_bikes['heure'] = df_bikes['heure'].replace('', np.nan)
 df_bikes.dropna(subset=['heure'], inplace=True)
 
 time_improved = pd.to_datetime(df_bikes['date'] +
                                ' ' + df_bikes['heure'] + ':00',
                                format='%Y-%m-%d %H:%M')
 df_bikes['Time'] = time_improved
-df_bikes.set_index('Time',inplace=True)
+df_bikes.set_index('Time', inplace=True)
 df_bikes['existence securite'] = df_bikes['existence securite'].replace(np.nan, "Inconnu")
 del df_bikes['identifiant vehicule']
 del df_bikes['type autres vehicules']
@@ -49,7 +47,7 @@ del df_bikes['categorie usager']
 del df_bikes['age']
 
 
-#security device histogram
+# Security device histogram
 plt.figure()
 df_bikes.groupby('existence securite').size().plot(kind='bar')
 plt.xlabel("Security device")
@@ -57,7 +55,7 @@ plt.ylabel("Number of accidents")
 plt.title("Breakdown of accidents by security's type")
 plt.savefig('security_devices.pdf', bbox_inches="tight")
 
-#gender
+# Gender
 sns.set_palette("Set2", n_colors=12)
 plt.figure()
 df_bikes.groupby('sexe').size().plot(kind='bar')
@@ -66,7 +64,7 @@ plt.ylabel("Number of accidents")
 plt.title("Accidents per gender")
 plt.savefig('Gender.pdf', bbox_inches="tight")
 
-#death's gender
+# Death and gender
 df_deads = df_bikes[df_bikes['gravite accident']=='3 - Tué']
 df_deads.groupby('sexe').size().plot(kind='bar')
 plt.xlabel("Gender")
@@ -74,12 +72,12 @@ plt.ylabel("Number of deaths")
 plt.title("Deaths per gender")
 plt.savefig('Gender_deaths.pdf', bbox_inches="tight")
 
-#We will need this later
+# We will need this later
 df_bikes['month'] = df_bikes.index.month
-df_bikes['hour']=df_bikes.index.hour
-df_bikes['year']=df_bikes.index.year
+df_bikes['hour'] = df_bikes.index.hour
+df_bikes['year'] = df_bikes.index.year
 
-#convert gravity and sex into quantitative variable
+# Convert gravity and sex into quantitative variable
 df_bikes['grave_quanti'] = df_bikes['gravite accident']
 df_bikes['grave_quanti'] = df_bikes['grave_quanti'].replace("0 - Indemne", 0)
 df_bikes['grave_quanti'] = df_bikes['grave_quanti'].replace("1 - Blessé léger", 1)
@@ -90,18 +88,18 @@ df_bikes['sex_quanti'] = df_bikes['sexe']
 df_bikes['sex_quanti'] = df_bikes['sex_quanti'].replace("M", 0)
 df_bikes['sex_quanti'] = df_bikes['sex_quanti'].replace("F", 1)
 
-#try to predict with qualitative variables into ordinal variables
-#predict gravity of accident with sex (bad try)
+# Try to predict with qualitative variables into ordinal variables
+# predict gravity of accident with sex (bad try)
 
 interest = ['grave_quanti', 'sex_quanti']
-df_bikes_ols = df_bikes[interest].dropna(how = 'any') #drop all Nan values
+df_bikes_ols = df_bikes[interest].dropna(how='any')  # Drop all Nan values
 df_bikes_ols
 
-#sampling
+# Sampling
 df_test = df_bikes_ols.iloc[0:1000]
 df_sample = df_bikes_ols.iloc[1000:len(df_bikes_ols)]
 
-#model with qualitative variables
+# Model with qualitative variables
 results = smf.ols('grave_quanti ~ sex_quanti', data=df_sample).fit()
 
 plt.figure()
@@ -114,11 +112,11 @@ plt.xlim([-0.1, 1.1])
 plt.legend()
 plt.savefig('severitypredictionwithsex.pdf', bbox_inches="tight")
 
-#new try on real quantitative variables
-#let's focus on the number of accidents
+# Mew try on real quantitative variables
+# Let's focus on the number of accidents
 
-#daily profile of the number of accidents per month
-import calendar
+# Daily profile of the number of accidents per month
+
 df_bikes['month'] = df_bikes.index.month
 df_bikes['month'] = df_bikes['month'].apply(lambda x: calendar.month_abbr[x])
 
@@ -136,29 +134,32 @@ axes.set_title(
     "Daily profile of the number of accidents per month")
 axes.set_xticks(np.arange(0, 24))
 axes.set_xticklabels(np.arange(0, 24), rotation=45)
-axes.legend(labels=calendar.month_name[1:], loc='lower left', bbox_to_anchor=(1, 0.1))
+axes.legend(labels=calendar.month_name[1:], loc='lower left',
+            bbox_to_anchor=(1, 0.1))
 plt.savefig('Daily_profile.pdf', bbox_inches="tight")
 plt.tight_layout()
 
-#new study, reset dataset
-df_bikes = pd.read_csv(path_target2, na_values="", converters={'data': str, 'heure': str})
+# New study, reset dataset
+df_bikes = pd.read_csv(path_target2, na_values="",
+                       converters={'data': str, 'heure': str})
 df_bikes["date"] = pd.to_datetime(df_bikes["date"])
 df_bikes = df_bikes.sort_values(["date"])
 
 df_bikes_date = pd.DataFrame(df_bikes.groupby(
     "date").count()["identifiant accident"])
 
-df_bikes_date['date']=df_bikes_date.index
-df_bikes_date.rename(columns = {'identifiant accident': 'accidents'}, inplace=True)
+df_bikes_date['date'] = df_bikes_date.index
+df_bikes_date.rename(columns={'identifiant accident': 'accidents'},
+                     inplace=True)
 
-#features
-df_bikes_date['month']=df_bikes_date.index.month
-df_bikes_date['year']=df_bikes_date.index.year
-df_bikes_date['day']=df_bikes_date.index.day
+# Features
+df_bikes_date['month'] = df_bikes_date.index.month
+df_bikes_date['year'] = df_bikes_date.index.year
+df_bikes_date['day'] = df_bikes_date.index.day
 df_bikes_date['periodic_day'] = np.cos(df_bikes_date['day'])
 df_bikes_date['periodic_month'] = np.cos(df_bikes_date['month'])
 
-#sampling
+# Sampling
 df_sample = df_bikes_date.iloc[355:5100]
 df_test = df_bikes_date.iloc[0:355]
 
@@ -169,34 +170,36 @@ plt.scatter(df_test['date'], df_test['accidents'], label='Data')
 plt.xlabel('date')
 plt.ylabel('Number of accidents')
 plt.title('Prediction of the number of accidents')
-plt.plot(df_test['date'], results.predict(df_test), '--', color='red', label='OLS')
+plt.plot(df_test['date'],
+         results.predict(df_test), '--', color='red', label='OLS')
 plt.legend()
 plt.savefig('accidentprediction.pdf', bbox_inches="tight")
 
 
-#new dataset
-df_comptage = pd.read_excel(path_target1, engine="odf")
+# New dataset
+df_comptage = pd.read_excel(path_target1, engine="odf")  # dependence on odf
 df_comptage.columns.unique()
-variables = ['Date de passage', 'Heure de passage', 
-             'Nombre total de vélos passés depuis la mise en circulation', 'Nombre de vélos passés depuis le début de la journée']
+variables = ['Date de passage', 'Heure de passage',
+             'Nb total de vélos passés depuis la mise en circulation',
+             'Nb de vélos passés depuis le début de la journée']
 df_comptage = df_comptage[variables]
-df_comptage.rename(columns = {'Nombre total de vélos passés depuis la mise en circulation': 'Total', 
-                                  'Nombre de vélos passés depuis le début de la journée': 'Day_total'}, inplace=True)
+df_comptage.rename(columns={'Nb de vélos depuis lancement': 'Total',
+                            'Nb de vélos depuis le début de la journée': 'Day_total'}, inplace=True)
 
-df_comptage.Day_total[505]=0 #it was a str
+df_comptage.Day_total[505] = 0  # it was a str
 
-#create features
-df_comptage['date']=pd.to_datetime(df_comptage["Date de passage"], format='%Y-%m-%d')
-df_comptage_ag = df_comptage.groupby('date').aggregate({'Total' : 'max', 'Day_total': 'max'})
+# Create features
+df_comptage['date'] = pd.to_datetime(df_comptage["Date de passage"], format='%Y-%m-%d')
+df_comptage_ag = df_comptage.groupby('date').aggregate({'Total': 'max', 'Day_total': 'max'})
 
 df_comptage_ag['date'] = df_comptage_ag.index
 df_comptage_ag['month'] = df_comptage_ag.index.month
 df_comptage_ag['day'] = df_comptage_ag.index.day
-df_comptage_ag['num']=range(182)
+df_comptage_ag['num'] = range(182)
 df_comptage_ag['sinus_day'] = np.sin(df_comptage_ag['day'])
 df_comptage_ag['sinus_num'] = np.sin(df_comptage_ag['num'])
 
-#sampling
+# Sampling
 df_sample = df_comptage_ag.iloc[0:150]
 df_test = df_comptage_ag.iloc[150:182]
 
